72f9ad1209c0ec3b328923bc6d9e71a1bcfedd67
AM <arne_meier@freenet.de>
2023-11-05 11:13:27 +0100

    update depolyment

--- create_volume.sh
+++ create_volume.sh
@@ -1,2 +1,9 @@
-docker volume create --opt type=none --opt o=bind --opt device=~/data/chatbotvol-develop --name chatbotvol-develop
-chown 1001:1001 ~/data/chatbotvol-develop
+mkdir -p /home/bigmama/chatbot/data/chatbotvol-develop
+chown 1001:1001 /home/bigmama/chatbot/data/chatbotvol-develop
+docker volume create --opt type=none --opt o=bind --opt device=/home/bigmama/chatbot/data/chatbotvol-develop --name chatbotvol-develop
+mkdir -p /home/bigmama/chatbot/data/chatbotvol-staging
+chown 1001:1001 /home/bigmama/chatbot/data/chatbotvol-staging
+docker volume create --opt type=none --opt o=bind --opt device=/home/bigmama/chatbot/data/chatbotvol-staging --name chatbotvol-staging
+mkdir -p /home/bigmama/chatbot/data/chatbotvol-main
+chown 1001:1001 /home/bigmama/chatbot/data/chatbotvol-main
+docker volume create --opt type=none --opt o=bind --opt device=/home/bigmama/chatbot/data/chatbotvol-main --name chatbotvol-main
